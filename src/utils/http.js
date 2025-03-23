eac03ddcae7a85c035ec19adde024f08933d0b06
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-23 13:31:24 +0530

    ⚙︎ Implement event search functionality & Added loading and error handling...

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,11 @@
-export const fetchEvents = async () => {
-   const response = await fetch("http://localhost:3000/events");
+export const fetchEvents = async ({ searchTerm, signal }) => {
+   let url = "http://localhost:3000/events";
+
+   if (searchTerm) {
+      url += `?search=${searchTerm}`;
+   }
+
+   const response = await fetch(url, { signal });
 
    if (!response.ok) {
       const error = new Error("An error occurred while fetching the events");
