a60bb46fbc3469e27dcd2e5e05899e5245ba2226
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-23 13:58:23 +0530

    ⇪ Update FindEventSection to use isLoading state and enable query based on search term...

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -9,9 +9,10 @@ export default function FindEventSection() {
    const searchElement = useRef();
    const [searchTerm, setSearchTerm] = useState("");
 
-   const { data, isPending, isError, error } = useQuery({
+   const { data, isLoading, isError, error } = useQuery({
       queryKey: ["events", { search: searchTerm }],
       queryFn: ({ signal }) => fetchEvents({ searchTerm, signal }),
+      enabled: searchTerm.length > 0,
       // staleTime: 5000,
    });
 
@@ -22,7 +23,7 @@ export default function FindEventSection() {
 
    let content = <p>Please enter a search term and to find events.</p>;
 
-   if (isPending) {
+   if (isLoading) {
       content = <LoadingIndicator />;
    }
 
